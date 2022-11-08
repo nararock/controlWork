021acd5cd00a6cdf5b7e14ca2367ead7b4d64ffe
Elena Gerasimovich <nararock@mail.ru>
2022-11-08 16:43:20 +0300

    create array

---
+++ controlWork/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] stringArray = createArray();
+Console.WriteLine(String.Join(",", stringArray));
+
+string[] createArray(){
+    Console.WriteLine("Введите размер массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[size]; 
+    Console.WriteLine("Заполним массив строками");
+    for (int i = 0; i < size; i++){
+        Console.Write("Введите строку: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
